7108de4574dbb1345b76771b92e5c57f260847e2
Akash Dhangar <aakashdhangar2k10@gmail.com>
2025-05-27 02:11:43 +0530

    api-update code in api-update.php file

---
+++ api-update.php
@@ -0,0 +1,26 @@
+<?php 
+header('Content-Type: application/json');
+header('Access-Content-Allow-Origin: *');
+header('Access-Content-Allow-Methods: POST');
+header('Access-Content-Allow-Headers:Access-Content-Allow-Headers,Content-Type,Access-Content-Allow-Methods,Authorization,X-Requested-With');
+
+
+
+$data = json_decode(file_get_contents("php://input"),true);
+
+$id  = $data['id'];
+$name  = $data['name'];
+$age  = $data['age'];
+$city  = $data['city'];
+
+include 'config.php';
+
+$sql = "UPDATE students SET name = '{$name}', age = '{$age}', city = '{$city}' WHERE id = '{$id}"; 
+
+if(mysqli_query($conn,$sql)){
+    echo json_encode(array('message' => 'Student Record Updated.', 'status' => True));
+}
+else{
+    echo json_encode(array('message' => 'Student Record Not Updated.', 'status' => false));
+}
+?>
