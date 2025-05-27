ddec05c544a72095a325c34ccb8eba8942323540
Akash Dhangar <aakashdhangar2k10@gmail.com>
2025-05-28 00:58:29 +0530

    Add Code in api-search.php file to search by GET Methord

---
+++ api-search.php
@@ -0,0 +1,24 @@
+<?php 
+header('Content-Type: application/json');
+header('Access-Content-Allow-Origin: *');
+include 'config.php';
+
+// $data = json_decode(file_get_contents("php://input"),true);
+// $search  = $data[' search'];
+
+$search= isset($_GET['search'])? $_GET['search']:die();
+
+$sql = "SELECT * FROM students WHERE name LIKE '%{$search}%'";
+$result = mysqli_query($conn,$sql) or die("SQL Query Failed.");
+
+if(mysqli_num_rows($result)>0){
+
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+}
+else{
+    echo json_encode(array('message' => 'No Record Found.', 'status' => false));
+}
+
+
+?>
